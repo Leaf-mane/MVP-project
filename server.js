bc72401f1f2af0fae0c696aa40496398ec0ea709
NOAH MORRISON <morrison.noah@gmail.com>
2023-11-16 11:24:38 -0800

    Changed to pg from postgres

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 import express from "express";
-import postgres from "postgres";
+import pg from "pg";
 import dotenv from "dotenv";
 
-
-/////////////////////////////////////////////////////
 dotenv.config();
 const app = express();
-const sql = postgres(process.env.DATABASE_URL);
+const { Pool } = pg;
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
 
 app.use(express.json());
 app.use(express.static("public"));
-//////////////////////////////////////////////////////
+
 app.get('/employees', async (req, res) => {
     try {
-      const employees = await sql`SELECT * FROM employees`;
-      res.json(employees);
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM employees');
+      const employees = result.rows;
+      client.release();
+      res.status(200).json(employees);
     } catch (error) {
       res.status(500).json({ error: 'Unable to fetch employees' });
     }
