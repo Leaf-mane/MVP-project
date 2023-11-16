2d533d83dec395151965049f7ca67e43c37aa76e
NOAH MORRISON <morrison.noah@gmail.com>
2023-11-16 09:08:06 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import postgres from "postgres";
+import dotenv from "dotenv";
+
+
+/////////////////////////////////////////////////////
+dotenv.config();
+const app = express();
+const sql = postgres(process.env.DATABASE_URL);
+
+app.use(express.json());
+app.use(express.static("public"));
+//////////////////////////////////////////////////////
+app.get('/employees', async (req, res) => {
+    try {
+      const employees = await sql`SELECT * FROM employees`;
+      res.json(employees);
+    } catch (error) {
+      res.status(500).json({ error: 'Unable to fetch employees' });
+    }
+  });
+
+
+
+
+
+
+
+
+// Listener
+const start = () => {
+    app.listen(port, () => {
+        console.log(`Server is now running on port ${port}`);
+    });
+};
+
+start();
